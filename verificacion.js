d1243e058fd43caa04ee75a2fd7829cd07917e67
zomby43 <psycho432@gmail.com>
2022-06-01 01:25:18 -0400

    Formulario y Validacion

---
+++ verificacion.js
@@ -0,0 +1,29 @@
+function validar(){
+    var telefono = document.Formulario.inputTelefono.value
+    var nombre = document.Formulario.inputName.value
+    var correo = document.Formulario.inputCorreo.value
+    var contraseña = document.Formulario.inputContraseña.value
+    var rut = document.Formulario.inputRut.value
+    if(nombre.length<3){
+        alert("Nombre debe Tener 3 o mas caracteres")
+        return false;
+    }
+    if(correo.length<3){
+        alert("Correo debe Tener 3 o mas caracteres")
+        return false;
+    }
+    if(contraseña.length<3){
+        alert("Contraseña debe tener 3 o mas caracteres")
+        return false;
+    }
+    if (telefono.length<9){
+    alert("Telefono debe contener 9 caracteres")
+    return false
+    }
+    if (rut.length<9){
+        alert("Rut debe contener 9 caracteres")
+        return false
+        }
+    cadena = "Nombre: "+nombre+"\nCorreo: "+correo+"\nContraseña: "+contraseña+"\nTelefono:"+telefono+"\nRut:"+rut
+    alert(cadena)
+}
